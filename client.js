2834c5427b5b13321ca1826344c92c1c6bebd25b
Anup Parekh <aaparekh@ads.aexp.com>
2016-06-15 21:03:14 +0100

    comment

--- client.js
+++ client.js
@@ -110,7 +110,7 @@ function createScoreTable(teams) {
 		console.log(teams[x].name + " status: " + teams[x].active);
 		if(teams[x].active) {
 			newColour = lighten(teams[x].colour, DARKEN);
-			scores = scores + "<td style=\"background-color:" + teams[x].colour +"; color:white; border-radius:10px; width:70px; line-height:70px; box-shadow: inset 0 -10px 1px"+newColour+";\"><span class=\"scoreNum\">" + teams[x].score + "</span><span class=\"scoreTarget\" style=\"color:"+newColour+"\">/" + teams[x].target + "</span></td>";		
+			scores = scores + "<td style=\"background-color:" + teams[x].colour +"; color:white; border-radius:5px; width:70px; line-height:70px; box-shadow: inset 0 -10px 1px"+newColour+";\"><span class=\"scoreNum\">" + teams[x].score + "</span><span class=\"scoreTarget\" style=\"color:"+newColour+"\">/" + teams[x].target + "</span></td>";		
 			//Add in another indicator for this team
 		} else {
 			scores = scores + "<td style=\"width:70px; line-height:70px; color:" + teams[x].colour +"\"><span class=\"scoreNum\">" + teams[x].score + "</span><span class=\"scoreTarget\">/" + teams[x].target + "</span></td>";		
@@ -354,6 +354,10 @@ function connect () {
 			if(spyMasterMode) {
 				console.log ("Spymaster active");
 				$("#clue").html("<input class=\"dynamic\" id=\"clueBox\" type=\"text\" placeholder=\"Enter Brand...\"></input><div id=\"numInput\">"+numSelector+"</div><div id=\"numTxt\">products linked</div><input type=\"button\" id=\"giveClue\" value=\"Send\" onclick=\"giveClue()\"></input>");
+				document.styleSheets[0].addRule('.dynamic::-webkit-input-placeholder', 'color:'+teamColour);
+				document.styleSheets[0].addRule('.dynamic:-moz-placeholder', 'color:'+teamColour);
+				document.styleSheets[0].addRule('.dynamic::-moz-placeholder', 'color:'+teamColour);
+				document.styleSheets[0].addRule('.dynamic:-ms-input-placeholder', 'color:'+teamColour);
 				//var index = data.teams.indexOf(turn);
 				//var index = data.teams.map(function(e) { return e.name; }).indexOf(turn);
 				$('#giveClue').css('background-color', teamColour);
