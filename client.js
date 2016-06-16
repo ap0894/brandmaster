228e757bbc7c7f7113b3aa7843b26bfa64136a40
Anup Parekh <aaparekh@ads.aexp.com>
2016-06-16 09:31:33 +0100

    comment

--- client.js
+++ client.js
@@ -424,6 +424,10 @@ function clicked(value){
 	if(!spyMasterMode) {
 		if (activeGo && goes > 0) {
 			var word = document.getElementById(value).getElementsByTagName('a')[0].innerHTML;
+			confirmModal.style.display = "block";
+			$('.modal-content').css('background-color', 'white');
+			//newColour = lighten('#2ec306', DARKEN);
+			$('.modal-content').css('box-shadow', 'inset 0 -5px 1px' + COLOR_GREY);
 			if (window.confirm("Are you sure you want to select '"+word+"'?")) {
 				//only deactivate once reached max goes
 				if (goes == 0) {
