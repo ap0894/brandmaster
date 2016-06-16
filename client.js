c1c31914dc2c8931854ddd9d8aee88cb82327ce7
Anup Parekh <anup.parekh@aexp.com>
2016-06-16 21:19:38 +0100

    comment

--- client.js
+++ client.js
@@ -204,7 +204,8 @@ function connect () {
 			$('#clueBox').val(data.clue.toUpperCase() + " " + data.num);
 			$('#clueBox').css('border', '1px solid ' + teamColour);
 		}
-		$('#history').append(turn + " team set clue " + data.clue.toUpperCase() + " (" + data.num + ")<br />");
+		
+		$('#history').append("<span style=\"color:"+activeTeamColour+"\">" + toTitleCase(turn) + "</span> team set clue <span style=\"color:"+activeTeamColour+"\">" + data.clue.toUpperCase() + "</span> (" + data.num + ")<br />");
 		// Activate players
 		if(room == data.whoseGo && !spyMasterMode) {
 			console.log("Activating players in active team except the spymaster");
