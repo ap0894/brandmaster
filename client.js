21386c01d20d88007a2784134f8f822d8afdaa25
Anup Parekh <aaparekh@ads.aexp.com>
2016-06-16 10:10:19 +0100

    comment

--- client.js
+++ client.js
@@ -120,7 +120,7 @@ function createScoreTable(teams) {
 		console.log(teams[x].name + " status: " + teams[x].active);
 		if(teams[x].active) {
 			newColour = lighten(teams[x].colour, DARKEN);
-			scores = scores + "<td style=\"background-color:" + teams[x].colour +"; color:white; border-radius:5px; width:70px; line-height:70px; box-shadow: inset 0 -10px 1px"+newColour+";\"><span class=\"scoreNum\">" + teams[x].score + "</span><span class=\"scoreTarget\" style=\"color:"+newColour+"\">/" + teams[x].target + "</span></td>";		
+			scores = scores + "<td style=\"background-color:" + teams[x].colour +"; color:white; border-radius:5px; width:70px; line-height:70px; box-shadow: inset 0 -5px 1px"+newColour+";\"><span class=\"scoreNum\">" + teams[x].score + "</span><span class=\"scoreTarget\" style=\"color:"+newColour+"\">/" + teams[x].target + "</span></td>";		
 			//Add in another indicator for this team
 		} else {
 			scores = scores + "<td style=\"width:70px; line-height:70px; color:" + teams[x].colour +"\"><span class=\"scoreNum\">" + teams[x].score + "</span><span class=\"scoreTarget\">/" + teams[x].target + "</span></td>";		
