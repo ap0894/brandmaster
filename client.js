2acd15eb33bd60a8e51525fd534ceb9f01df1ad9
Anup Parekh <anup.parekh@aexp.com>
2016-06-17 22:40:44 +0100

    comment

--- client.js
+++ client.js
@@ -218,7 +218,7 @@ function connect () {
 		loc = "images/avatar_"+room+"_small.png"
 		$('#avatar').attr("src",loc);
 		teamName = toTitleCase(room).slice(0,-1);
-		$('#avatarContainer').append("<br/><span style=\"color:"+teamColour+"\">"+teamName+"</span>");
+		$('#avatarContainer').append("<br/><span style=\"color:"+teamColour+"\">"+teamName+" team</span>");
 		//var fill;
 	});
 
