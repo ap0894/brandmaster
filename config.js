e647ac22153c66a026225e00dffb1ab0ac765b55
Anup Parekh <Anup@Anups-MacBook-Pro.local>
2017-05-27 19:06:52 +0100

    comment

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var Config = {
   // Contents of this file will be send to the client
-  //"domain":     process.env.OPENSHIFT_APP_DNS || '127.0.0.1',
+  "domain":     process.env.OPENSHIFT_APP_DNS || '127.0.0.1',
 
   "serverip":   process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
   "serverport": process.env.PORT || 5000,
